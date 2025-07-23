f0141f8b119d9e45cd4ed1129295bf8227b2f216
unknown <mariellecanonigo@gmail.com>
2025-07-23 01:37:17 -0700

    first push

---
+++ src/bonghanoyproj/BonghanoyProj.java
@@ -0,0 +1,26 @@
+
+package bonghanoyproj;
+
+import java.util.Scanner;
+
+public class BonghanoyProj {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        
+        System.out.print("Enter brth year: ");
+        int byear = sc.nextInt();
+        
+        System.out.println("\nHello "+name+" your age is "+(2025-byear));
+        
+        if ((2025-byear) >= 18){
+            System.out.println("Votable");
+        }else{
+            System.out.println("Minority");
+    }
+    }
+    
+}
